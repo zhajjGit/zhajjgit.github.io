43c15b7c69800c8b0be1df276764485b93089b12
zhajj <254020031@qq.com>
2019-04-28 19:35:26 +0800

    Update topScroll.js

--- assets/js/topScroll.js
+++ assets/js/topScroll.js
@@ -4,7 +4,7 @@ cancelAnimationFrame(timer);
 timer = requestAnimationFrame(function fn(){
 var oTop = document.body.scrollTop || document.documentElement.scrollTop;
 if(oTop > 0){
-document.body.scrollTop = document.documentElement.scrollTop = oTop - 300;
+document.body.scrollTop = document.documentElement.scrollTop = oTop - 1000;
 timer = requestAnimationFrame(fn);
 }else{
 cancelAnimationFrame(timer);
