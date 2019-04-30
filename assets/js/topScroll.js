1ba4d95654758b49ffa60cde88bda950e003c904
zhajj <254020031@qq.com>
2019-04-30 12:07:28 +0800

    1

--- assets/js/topScroll.js
+++ assets/js/topScroll.js
@@ -1,13 +1,13 @@
 var timer = null;
-topBox.onclick = function(){
-cancelAnimationFrame(timer);
-timer = requestAnimationFrame(function fn(){
-var oTop = document.body.scrollTop || document.documentElement.scrollTop;
-if(oTop > 0){
-document.body.scrollTop = document.documentElement.scrollTop = oTop - 1000;
-timer = requestAnimationFrame(fn);
-}else{
-cancelAnimationFrame(timer);
-} 
-});
+	topBox.onclick = function(){
+		cancelAnimationFrame(timer);
+		timer = requestAnimationFrame(function fn(){
+			var oTop = document.body.scrollTop || document.documentElement.scrollTop;
+			if(oTop > 0){
+			document.body.scrollTop = document.documentElement.scrollTop = oTop - 1000;
+			timer = requestAnimationFrame(fn);
+		}else{
+			cancelAnimationFrame(timer);
+		} 
+	});
 }
